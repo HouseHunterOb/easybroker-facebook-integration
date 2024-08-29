5ac4cdbd5fe1eada20c702dd7dc0679d91c1fbac
Diego Jonguitud G <diegojonguitud@househunter.com.mx>
2024-08-29 10:09:00 -0600

    Fix API Keys

--- config.js
+++ config.js
@@ -3,9 +3,13 @@ require('dotenv').config(); // Carga las variables de entorno desde .env
 module.exports = {
     facebook: {
         accessToken: process.env.FACEBOOK_ACCESS_TOKEN,
-        pageId: process.env.FACEBOOK_PAGE_ID
+        // Puedes agregar el ID de la página de Facebook aquí si lo necesitas
+        // pageId: process.env.FACEBOOK_PAGE_ID, 
     },
     easyBroker: {
-        apiKey: process.env.EB_API_KEY
+        apiKey: process.env.EASYBROKER_API_KEY
+    },
+    openAI: {
+        apiKey: process.env.OPENAI_API_KEY
     }
 };
